f23327bcf4f9db214de6a464b29c901226aba2ef
Simone <tritto.simone@gmail.com>
2023-09-14 12:21:31 +0200

    validation_dateBirth

--- person.py
+++ person.py
@@ -3,8 +3,15 @@ class Person:
     def __init__(self, name, surname, birthDate, univoqueCode):
         self.name=name
         self.surname=surname
-        self.birthDate=birthDate
+        if self.__validation_date(birthDate):
+            self.birthDate=birthDate
+        else:
+            raise TypeError('Data non valida')
         self.univoqueCode=univoqueCode
+
+    def __validation_date(self, birthDate):
+        return isinstance(birthDate, datetime) and birthDate<datetime.now()
+        
 
     @property
     def age(self):
@@ -16,6 +23,13 @@ class Person:
         return self.age>=18         # chiama property age con .age
 
 
-# Esempio aggiunta Person
+# *********Esempio aggiunta Person con costrutto try except
+# try:
+#     gigi=Person('Gigi', 'Rossi', datetime(1995, 12, 21), 1)
+#     print(f'{gigi.name} ha {gigi.age} anni')
+# except:
+#     print('Errore')
+
+# **********Esempio aggiunta Person
 # data_nascita=datetime(1999,10,20)
 # pp=Person('Luigi','Bianchi', data_nascita, 1)
